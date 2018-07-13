3f80c6696c7f69183dbe061b4ad848337d02b977
Neil Stevens <neil@stevensmedia.us>
2018-07-12 21:22:54 -0400

    Grunt task for creating icon.  Fix electron call to work.  Get out of app/ dir since electron doesn't actually need it

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,34 @@
+var execSync = require('child_process').execSync;
+
+function createIcnsFile(grunt, source, name, location) {
+	var iconset = 'build/' + name + '.iconset';
+	execSync('mkdir -p ' + iconset);
+
+	var sizes = {};
+	for(size of [16, 32, 128, 256, 512]) {
+		sizes['' + size + 'x' + size] = size;
+	}
+	for(size of [16, 32, 128, 256, 512]) {
+		sizes['' + size + 'x' + size + '@2x'] = size * 2;
+	}
+
+	for(key in sizes) {
+		var val = sizes[key];
+		var cmd = 'sips -z ' + val + ' ' + val + ' ' +
+		           source +
+		           ' --out ' + iconset + '/' +
+		           name + '_' + key + '.png';
+		grunt.log.write(cmd + "\n");
+		execSync(cmd);
+	}
+
+	var cmd = 'iconutil -c icns ' +
+	          '--output ' + location + '/' + name + '.icns '
+	          + iconset;
+	grunt.log.write(cmd + "\n");
+	execSync(cmd);
+}
+
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-electron');
@@ -8,12 +39,12 @@ module.exports = function(grunt) {
 			mac: {
 				options: {
 					name: 'Livesplit One',
-					dir: 'app',
-					icon: 'app/icon.png',
+					dir: '.',
+					icon: 'build/icon',
 					out: 'build',
 					ignore: 'build',
 					platform: 'darwin',
-					arch: 'x64'
+					arch: 'x64',
 				}
 			}
 		},
@@ -24,8 +55,14 @@ module.exports = function(grunt) {
 
 	var default_tasks = [
 		'clean',
+		'appicon',
 		'electron:mac'
 	];
+
+	grunt.registerTask('appicon', 'Create app icon', function() {
+		grunt.log.write("Creating app icon\n");
+		createIcnsFile(grunt, 'icon.png', 'icon', 'build');
+	});
 
 	grunt.registerTask('default', default_tasks);
 };
