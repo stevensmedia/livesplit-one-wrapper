877540c478ad062f3c3d2f42af6b5abdea9c108f
Neil Stevens <neil@stevensmedia.us>
2018-07-15 21:52:55 -0400

    Making the buttons and timer also full width responsive

--- oob.js
+++ oob.js
@@ -5,6 +5,14 @@ function applyStyle() {
 	}
 	$('.livesplit-container > div').css('margin', '0');
 	$('.layout').css('width', 'auto');
+	$('.buttons').css('width', 'auto');
+	$('.buttons').css('text-align', 'center');
+	$('.buttons > .small').css('display', 'inline-block');
+	$('.buttons > .small').css('width', '48%');
+	$('.buttons > .small > button').css('width', '48%');
+	$('svg.Timer0').attr('viewBox', '0 0 299 60')
+	               .attr('preserveAspectRatio', 'xMaxYMin meet')
+	               .css('width', '100%');
 }
 
 var $ = require('jquery');
