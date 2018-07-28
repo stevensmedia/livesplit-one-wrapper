7842732a790f80dafefc888a0bbfd22440431c32
Neil Stevens <neil@stevensmedia.us>
2018-07-28 00:36:13 -0400

    Comment up the code

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ var app = electron.app;
 var win;
 
 app.on('ready', function() {
+	/* MacOS dock icon */
 	app.dock.setIcon(path.join(__dirname, 'icon.png'));
+
+	/* We saved our last window size. Apply it. */
 	var size = settings.get('size');
 
+	/* Main window */
 	win = new electron.BrowserWindow({
 		width: size ? size[0] : 900,
 		height: size ? size[1] : 800,
@@ -16,6 +20,7 @@ app.on('ready', function() {
 		icon: path.join(__dirname, 'icon.png'),
 	});
 
+	/* This injects our custom JS into one.livesplit.org */
 	win.webContents.on('dom-ready', function() {
 		win.webContents.executeJavaScript(`
 			const path = require('path');
@@ -25,18 +30,22 @@ app.on('ready', function() {
 		`);
 	});
 
+	/* Make sure to save that window size for next time */
 	win.on('close', function() {
 		settings.set('size', win.getSize());
 	});
 
+	/* This function passes a click to a selector the render process */
 	var click = function(target) {
 		var encoded = JSON.stringify(target);
 		win.webContents.executeJavaScript(`
 			window.$(${encoded}).click();
+			/* Electron can't handle jQuery. Return something simple. */
 			0;
 		`);
 	};
 
+	/* This function passes a click to a selector's PARENT in the render process */
 	var clickParent = function(target) {
 		var encoded = JSON.stringify(target);
 		win.webContents.executeJavaScript(`
@@ -45,6 +54,7 @@ app.on('ready', function() {
 		`);
 	};
 
+	/* Global hotkeys*/
 	electron.globalShortcut.register('Shift+Down', function() {
 		click('.livesplit-container .layout');
 	});
@@ -61,10 +71,11 @@ app.on('ready', function() {
 		clickParent('i.fa-arrow-up');
 	});
 
-
+	/* Go! */
 	win.loadURL('https://one.livesplit.org/');
 });
 
+/* Goodbye */
 app.on('window-all-closed', function() {
 	app.quit()
 });
--- oob.js
+++ oob.js
@@ -1,31 +1,61 @@
-var applyStyleBound = false;
-function applyStyle() {
-	if(!$('.livesplit-container').length) {
-		setTimeout(applyStyle, 1);
-		return;
-	}
-	window.onbeforeunload = null;
+const ipcRenderer = require('electron').ipcRenderer;
+var $ = window.$ = require('jquery');
+
+function oobCSSChanges() {
+	/* The window doesn't need a margin */
 	$('.livesplit-container > div').css('margin', '0');
+
+	/* Splits should go full width */
 	$('.layout').css('width', 'auto');
+	$('svg.Timer0').attr('viewBox', '0 0 300 60')
+	               .attr('preserveAspectRatio', 'xMaxYMin meet')
+	               .css('width', '100%');
+
+	/* completely revamping the buttons */
 	$('.buttons').css('width', 'auto');
 	$('.buttons').css('text-align', 'center');
 	$('.buttons').css('position', 'fixed');
 	$('.buttons').css('bottom', '0%');
 	$('.buttons').css('width', '100%');
 	$('.buttons > .small').css('display', 'inline-block');
-	$('.buttons > .small').css('width', '48%');
+	$('.buttons > .small').css('width', '30%');
 	$('.buttons > .small > button').css('width', '48%');
-	$('svg.Timer0').attr('viewBox', '0 0 300 60')
-	               .attr('preserveAspectRatio', 'xMaxYMin meet')
-	               .css('width', '100%');
-
-	if(!applyStyleBound) {
-		applyStyleBound = true;
-		$('.livesplit-container').bind('DOMSubtreeModified', function(e) {
-			applyStyle();
-		});
-	}
 }
 
-var $ = window.$ = require('jquery');
-applyStyle();
+function oobSetup() {
+	/* Keep trying until LSO is actually loaded */
+	if(!$('.livesplit-container').length) {
+		setTimeout(oobSetup, 1);
+		return;
+	}
+
+	/* We need to do this so the window will actually be closeable */
+	window.onbeforeunload = null;
+
+	/* Apply CSS */
+	oobCSSChanges();
+
+	/* We need to re-apply the CSS on every load */
+	$('.livesplit-container').bind('DOMSubtreeModified', function(e) {
+		oobCSSChanges();
+	});
+
+	/* Adding our own configuration dialog */
+	var dialog = '<div id="wrapper-dialog"></div>';
+	$('body').append($(dialog));
+
+	/* Adding our own buttons */
+	var buttons = '<div class="small"><button id="contextmenu-button">⚙️</button><button id="wrapper-button">⌘</button></div>';
+	$('.buttons').append($(buttons));
+	$('#contextmenu-button').click(function() {
+		console.log('contextmenu-button click');
+		$('.livesplit-contaner .layout').contextmenu();
+	});
+	$('#wrapper-button').click(function() {
+		console.log('contextmenu-button click');
+		$('.livesplit-contaner .layout').contextmenu();
+	});
+
+}
+
+oobSetup();
